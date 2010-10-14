3a1d8d972bd5f8e4de53c37b7e7046bb120f9010
dB <dblock@dblock.org>
2010-10-14 01:06:04 +0000

    Added Demo and fixed packaging. Added unit tests that enumerate events after fire.
    git-svn-id: https://log4jna.svn.codeplex.com/svn/trunk@57917 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ demo/src/org/apache/log4jna/nt/demo/Demo.java
@@ -0,0 +1,25 @@
+package org.apache.log4jna.nt.demo;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Demo {
+	
+	public static void main(String[] args) {
+		try {
+			PropertyConfigurator.configureAndWatch("log4j.properties");
+			Logger logger = LogManager.getLogger(Demo.class);
+			String message = "";
+			for(String arg : args) {
+				message += arg;
+				message += "\r\n";
+			}
+			logger.debug("debug: " + message);
+			logger.info("info: " + message);
+		} catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);			
+		}
+	}
+}
--- log4jna/src-test/org/apache/log4jna/nt/Win32EventLogAppenderTest.java
+++ log4jna/src-test/org/apache/log4jna/nt/Win32EventLogAppenderTest.java
@@ -18,10 +18,17 @@
 package org.apache.log4jna.nt;
 
 import junit.framework.TestCase;
-import org.apache.log4j.Logger;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Level;
+
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import com.sun.jna.platform.win32.Advapi32Util.EventLogIterator;
+import com.sun.jna.platform.win32.Advapi32Util.EventLogRecord;
+import com.sun.jna.platform.win32.Advapi32Util.EventLogType;
+import com.sun.jna.platform.win32.Kernel32;
+import com.sun.jna.platform.win32.WinNT;
 
 /**
  * 
@@ -32,6 +39,13 @@ import org.apache.log4j.BasicConfigurator;
  */
 public class Win32EventLogAppenderTest extends TestCase {
 
+	private Logger _logger = null;
+	
+	public void setUp() {
+		BasicConfigurator.configure(new Win32EventLogAppender());
+		_logger = Logger.getLogger(Win32EventLogAppenderTest.class);		
+	}
+	
 	/**
 	 * Clean up configuration after each test.
 	 */
@@ -39,19 +53,56 @@ public class Win32EventLogAppenderTest extends TestCase {
 		LogManager.shutdown();
 	}
 
-	/**
-	 * Simple test of Win32EventLogAppender.
-	 */
-	public void testSimple() {
-		BasicConfigurator.configure(new Win32EventLogAppender());
-		Logger logger = Logger
-				.getLogger("org.apache.log4j.win32.Win32EventLogAppenderTest");
-		int i = 0;
-		logger.debug("Message " + i++);
-		logger.info("Message " + i++);
-		logger.warn("Message " + i++);
-		logger.error("Message " + i++);
-		logger.log(Level.FATAL, "Message " + i++);
-		logger.debug("Message " + i++, new Exception("Just testing."));
+	public void testDebugEvent() {
+		String message = "log4jna debug message @ " + Kernel32.INSTANCE.GetTickCount();		
+		_logger.debug(message);
+		expectEvent(message, Level.DEBUG, EventLogType.Informational);
+	}
+	
+	public void testInfoEvent() {
+		String message = "log4jna info message @ " + Kernel32.INSTANCE.GetTickCount();		
+		_logger.info(message);
+		expectEvent(message, Level.INFO, EventLogType.Informational);		
+	}
+	
+	public void testWarnEvent() {
+		String message = "log4jna warn message @ " + Kernel32.INSTANCE.GetTickCount();		
+		_logger.warn(message);
+		expectEvent(message, Level.WARN, EventLogType.Warning);				
+	}
+	
+	public void testFatalEvent() {
+		String message = "log4jna fatal message @ " + Kernel32.INSTANCE.GetTickCount();		
+		_logger.log(Level.FATAL, message);
+		expectEvent(message, Level.FATAL, EventLogType.Error);				
+	}
+	
+	/*
+	public void testException() {
+		String message = "log4jna exception message @ " + Kernel32.INSTANCE.GetTickCount();		
+		_logger.debug(message, new Exception("testing exception"));
+		expectEvent(message, Level.DEBUG, EventLogType.Informational);		
+	}
+	*/
+	
+	private void expectEvent(String message, Level level, EventLogType eventLogType) {
+		EventLogIterator iter = new EventLogIterator(null, "Application", WinNT.EVENTLOG_BACKWARDS_READ);
+		try {
+			assertTrue(iter.hasNext());
+			EventLogRecord record = iter.next();
+			assertEquals("Log4jna", record.getSource());
+			assertEquals(eventLogType, record.getType());			
+			assertEquals(1, record.getRecord().NumStrings.intValue());
+			assertNull(record.getData());
+			String fullMessage = level + " " + Win32EventLogAppenderTest.class.getCanonicalName() + " - " + message;
+			String eventMessage = record.getStrings()[0].trim();
+			int levelMarker = eventMessage.indexOf(level.toString());
+			assertTrue("missing level marker in '" + eventMessage + "'", levelMarker >= 0);
+			String eventMessageWithoutLocation = eventMessage.substring(levelMarker);
+			assertEquals(fullMessage, eventMessageWithoutLocation);
+			System.out.println(record.getStrings()[0]);
+		} finally {
+			iter.close();
+		}
 	}
 }
