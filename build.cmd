4712c6f3abf7f69d5a00cda4d4412426cce6ba9d
dB <dblock@dblock.org>
2010-12-06 20:51:19 +0000

    Added build.cmd that sets up a build environment.
    git-svn-id: https://log4jna.svn.codeplex.com/svn/trunk@60721 81fbe566-5dc4-48c1-bdea-7421811ca204

---
+++ build.cmd
@@ -0,0 +1,26 @@
+@echo off
+setlocal ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+pushd %~dp0
+
+set ProgramFilesDir=%ProgramFiles%
+if NOT "%ProgramFiles(x86)%"=="" set ProgramFilesDir=%ProgramFiles(x86)%
+
+set VisualStudioCmd=%ProgramFilesDir%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat
+if EXIST "%VisualStudioCmd%" call "%VisualStudioCmd%"
+
+for /D %%n in ( "%ProgramFilesDir%\NUnit*" ) do (
+ set NUnitDir=%%~n
+)
+
+call ant.bat %*
+
+popd
+endlocal
+goto :EOF
+
+:Usage
+echo  Syntax:
+echo.
+echo   build [target]
+echo.
+goto :EOF
